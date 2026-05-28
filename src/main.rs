b3d0dc61fb664d174a033afd2f12a1d080f7d946
getcmdrolled <wetturtle45@gmail.com>
2026-05-28 19:00:15 +0000

    feat: autorenaming, release workflow, updated gitignore

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ use std::env::args;
 use serde_json::json;
 use anyhow::{Context, Result};
 use ruzstd;
+use ustr::ustr;
 
 fn main() -> Result<()> {
     let argument = args().nth(1).with_context(|| "No file provided!")?;
     let input = BufReader::new(File::open(argument).with_context(|| "Unable to read file")?);
     let dom = rbx_binary::from_reader(input).with_context(|| "No DOM in provided file")?;
 
-    let mut poly_instance = rbxl_to_poly_instance(dom.root(), &dom);
-    poly_instance.name = String::from("World"); // me no likee referring to the World as DataModel :skull:
+    let poly_instance = rbxl_to_poly_instance(dom.root(), &dom);
     let poly_json = json!({
         "Version": "2.0.9",
         "FileType": 0,
@@ -37,12 +37,28 @@ fn rbxl_to_poly_instance(rbxl_instance: &rbx_dom_weak::Instance, dom: &rbx_dom_w
     poly_instance.name = rbxl_instance.name.clone();
     poly_instance = poly::convert_class(rbxl_instance.class.clone(), poly_instance.clone());
 
+    // Hardcode DataModel and Workspace to be renamed to poly equivalents to avoid regeneration
+    if poly_instance.name == "DataModel" && poly_instance.class_name == ustr("World") { poly_instance.name = String::from("World"); }
+    if poly_instance.name == "Workspace" && poly_instance.class_name == ustr("Environment") { poly_instance.name = String::from("Environment"); }
+
     for i in rbxl_instance.properties.clone() {
         poly_instance = poly::convert_property(i, poly_instance.clone());
     }
 
+    let mut unique_names: Vec<String> = Vec::new();
     for i in rbxl_instance.children() {
-        let child = rbxl_to_poly_instance(dom.get_by_ref(*i).unwrap(), dom);
+        let mut child = rbxl_to_poly_instance(dom.get_by_ref(*i).unwrap(), dom);
+
+        if unique_names.contains(&child.name) {
+            let mut lowest_available = 2;
+            while unique_names.contains(&(child.name.clone() + &lowest_available.to_string())) {
+                lowest_available += 1;
+            }
+            child.name = child.name.clone() + &lowest_available.to_string();
+        }
+
+        unique_names.push(child.name.clone());
+
         poly_instance.children.push(child);
     }
 
